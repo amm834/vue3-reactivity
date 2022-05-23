9be2866e9ce8fdec9680b3d75b44ee912baa1ff3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-23 21:45:57 +0630

    vue3 reactivity

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import {toRefs} from "vue";
+
+let obj = $ref({
+  count: 0
+})
+
+function increment() {
+  obj.count++
+}
+
+let {count} =  toRefs(obj)
+
+</script>
+
+<template>
+  <div>
+    Count: {{ count }}
+    <button @click="increment">Add</button>
+  </div>
+</template>
